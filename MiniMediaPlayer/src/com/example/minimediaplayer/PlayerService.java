004fa341a1276c6e2f2b65afdaf0ac640a98e58c
Lauren Elder <laurenelder@fullsail.edu>
2014-09-05 13:23:06 -0400

    Added Base UI

---
+++ MiniMediaPlayer/src/com/example/minimediaplayer/PlayerService.java
@@ -0,0 +1,19 @@
+package com.example.minimediaplayer;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class PlayerService extends IntentService{
+
+	public PlayerService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
