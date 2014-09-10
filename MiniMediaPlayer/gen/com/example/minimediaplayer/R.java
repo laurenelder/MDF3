f937dca2b7381fa5fe2bb16f9bc175b75c3f013b
Lauren Elder <laurenelder@fullsail.edu>
2014-09-10 14:51:45 -0400

    Crashing During Handler Callback

--- MiniMediaPlayer/gen/com/example/minimediaplayer/R.java
+++ MiniMediaPlayer/gen/com/example/minimediaplayer/R.java
@@ -26,21 +26,27 @@ public final class R {
         public static final int ic_action_pause=0x7f020001;
         public static final int ic_action_play=0x7f020002;
         public static final int ic_action_previous=0x7f020003;
-        public static final int ic_action_stop=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int ic_notification=0x7f020006;
+        public static final int ic_action_repeat=0x7f020004;
+        public static final int ic_action_stop=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_notification=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f09000a;
         public static final int container=0x7f090000;
-        public static final int nextBtn=0x7f090005;
-        public static final int playBtn=0x7f090003;
-        public static final int previousBtn=0x7f090002;
-        public static final int songTitle=0x7f090001;
-        public static final int stopBtn=0x7f090004;
+        public static final int frag=0x7f090009;
+        public static final int nextBtn=0x7f090008;
+        public static final int playBtn=0x7f090006;
+        public static final int previousBtn=0x7f090004;
+        public static final int progressBar=0x7f090003;
+        public static final int repeatBtn=0x7f090005;
+        public static final int songTitle=0x7f090002;
+        public static final int stopBtn=0x7f090007;
+        public static final int thumbnailImage=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
