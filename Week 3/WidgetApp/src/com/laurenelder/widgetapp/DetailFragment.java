4623eb7a8976b51bc3a19c09056a18070cbd5058
Lauren Elder <laurenelder@fullsail.edu>
2014-09-15 21:46:37 -0400

    Week 3 Added Fragments

--- Week 3/WidgetApp/src/com/laurenelder/widgetapp/DetailFragment.java
+++ Week 3/WidgetApp/src/com/laurenelder/widgetapp/DetailFragment.java
@@ -1,5 +1,44 @@
 package com.laurenelder.widgetapp;
 
-public class DetailFragment {
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class DetailFragment extends Fragment {
+	
+	// Establish Variables
+	Context context;
+	private detailInterface detailActivity;
+	
+	public interface detailInterface {
+		
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		
+		context = getActivity();
+		
+		if (activity instanceof detailInterface) {
+			detailActivity = (detailInterface) activity;
+		} else {
+			throw new ClassCastException((activity.toString()) + 
+					"Did not impliment detailInterface");
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+		View detailView = inflater.inflate(R.layout.activity_details, container);
+		return detailView;
+	}
 }
